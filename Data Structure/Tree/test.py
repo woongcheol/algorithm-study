bfdf72a6706278ef2f3587f9191d34d339f21f46
Woongcheol Shin <1994_100@naver.com>
2022-02-04 12:13:59 +0900

    add data structure concept 26/33

--- Data Structure/Tree/test.py
+++ Data Structure/Tree/test.py
@@ -38,6 +38,7 @@ class NodeMgmt:
     def delete(self, value):
         searched = False
         self.parent = self.head
+        self.change_node_parent = self.head 
         self.current_node = self.head
 
         while self.current_node:
@@ -72,16 +73,43 @@ class NodeMgmt:
 
             # child node 2개인 node 삭제
             elif self.current_node.left != None and self.current_node.right != None:
+                self.delete_node = self.current_node
                 self.current_node = self.current_node.right
+                
 
-                while self.current_node.left == None and self.current_node.right == None:
+                while self.current_node.left != None:
+                    self.change_node_parent = self.current_node
                     self.current_node = self.current_node.left
 
-                if self.parent.left.value == value:
-                    self.parent.left = self.current_node
-                else:
-                    self.parent.right = self.current_node
 
+                if self.parent.left.value == value:
+                    
+                    # 추가 - 바꿀 노드의 Child Node가 있을 경우
+                    if self.current_node.right != None:
+                        self.change_node_parent.left = self.current_node.right
+                        self.parent.left = self.current_node
+                        self.current_node.left = self.delete_node.left
+                        self.current_node.right = self.delete_node.right
+                        
+                    else:
+                        self.change_node_parent.left = None
+                        self.parent.left = self.current_node
+                        self.current_node.left = self.delete_node.left
+                        self.current_node.right = self.delete_node.right
+                else:
+                    
+                    # 추가 - 바꿀 노드의 Child Node가 있을 경우
+                    if self.current_node.right:
+                        self.change_node_parent.left = self.current_node.right
+                        self.parent.right = self.current_node
+                        self.current_node.left = self.delete_node.left
+                        self.current_node.right = self.delete_node.right
+                        
+                    else:
+                        self.change_node_parent.left = None
+                        self.parent.right = self.current_node
+                        self.current_node.left = self.delete_node.left
+                        self.current_node.right = self.delete_node.right
 
             # child node 1개인 node 삭제
             elif self.current_node.left != None or self.current_node.right != None:
@@ -96,7 +124,11 @@ class NodeMgmt:
                         self.parent.left = self.current_node.right
                     else:
                         self.parent.right = self.current_node.right
+            
+        return '삭제가 완료되었습니다'
 
+# Leaf Node 삭제
+'''
 BST = NodeMgmt(Node(10))
 BST.insert(5)
 BST.insert(15)
@@ -114,3 +146,29 @@ print(BST.delete(3)) # None
 print(BST.search(3)) # False
 
 print(BST.search(5)) # True
+'''
+
+# Child Node가 2개인 경우 삭제
+BST = NodeMgmt(Node(10))
+BST.insert(7)
+BST.insert(15)
+BST.insert(6)
+BST.insert(8)
+BST.insert(13)
+BST.insert(18)
+BST.insert(11)
+BST.insert(12)
+BST.insert(16)
+BST.insert(19)
+BST.insert(17)
+
+print(BST.search(15)) # 삭제될 노드 - True 
+print(BST.search(16)) # 교체될 노드 - True 
+print(BST.search(17)) # 교체될 노드 자식 노드 - True 
+
+print(BST.delete(15)) # None
+print(BST.search(15)) # False
+
+print(BST.search(17)) # True
+print(BST.search(13)) # 교체된 노드의 자식 노드 - True
+print(BST.search(18)) # 교체된 노드의 자식 노드 - True

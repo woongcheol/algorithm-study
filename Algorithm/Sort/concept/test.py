1f91756cff88864d6e1f2346b623a0fe2da88133
Woongcheol Shin <1994_100@naver.com>
2022-02-07 19:31:04 +0900

    baekjoon 11726

--- Algorithm/Sort/concept/test.py
+++ Algorithm/Sort/concept/test.py
@@ -17,18 +17,31 @@
 # print(bubble_sort(data))
 
 # 2. Insert Sort
-import random
+# import random
 
-def insert_sort(data):
-    for idx in range(1, len(data)):
-        for idx2 in range(idx, 0, -1):
-            if data[idx2] < data[idx2-1]:
-                data[idx2], data[idx2-1] = data[idx2-1], data[idx2]
-            else:
-                break
-    return data
+# def insert_sort(data):
+#     for idx in range(1, len(data)):
+#         for idx2 in range(idx, 0, -1):
+#             if data[idx2] < data[idx2-1]:
+#                 data[idx2], data[idx2-1] = data[idx2-1], data[idx2]
+#             else:
+#                 break
+#     return data
 
 
-data = random.sample(range(30), 10)
-print(data)
-print(insert_sort(data))
+# data = random.sample(range(30), 10)
+# print(data)
+# print(insert_sort(data))
+
+
+# DP
+def squear(n):
+    for idx in range(2, n+1):
+        memory[idx] = memory[idx-1] + memory[idx-2]
+    return memory[n]
+
+n = int(input())
+memory = [0 for _ in range(0, n+1)]
+memory[0] = 1
+memory[1] = 1
+print(squear(n))

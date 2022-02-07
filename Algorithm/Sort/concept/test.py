6cdb945a1b49c437ab05fadf6ffde2209cd3f4ba
Woongcheol Shin <1994_100@naver.com>
2022-02-07 15:10:34 +0900

    add algorithm concept 4/52

--- Algorithm/Sort/concept/test.py
+++ Algorithm/Sort/concept/test.py
@@ -1,17 +1,34 @@
-# Bubble Sort
+# 1. Bubble Sort
+# import random
+
+# def bubble_sort(data):
+#     for idx in range(0, len(data)-1):
+#         sorted = False
+#         for idx2 in range(len(data)-idx-1):
+#             if data[idx2] > data[idx2+1]:
+#                 data[idx2], data[idx2+1] = data[idx2+1], data[idx2]
+#                 sorted = True
+#         if sorted == False:
+#             break
+#     return data
+
+# data = random.sample(range(30), 10)
+# print(data)
+# print(bubble_sort(data))
+
+# 2. Insert Sort
 import random
 
-def bubble_sort(data):
-    for idx in range(0, len(data)-1):
-        sorted = False
-        for idx2 in range(len(data)-idx-1):
-            if data[idx2] > data[idx2+1]:
-                data[idx2], data[idx2+1] = data[idx2+1], data[idx2]
-                sorted = True
-        if sorted == False:
-            break
+def insert_sort(data):
+    for idx in range(1, len(data)):
+        for idx2 in range(idx, 0, -1):
+            if data[idx2] < data[idx2-1]:
+                data[idx2], data[idx2-1] = data[idx2-1], data[idx2]
+            else:
+                break
     return data
+
 
 data = random.sample(range(30), 10)
 print(data)
-print(bubble_sort(data))
+print(insert_sort(data))

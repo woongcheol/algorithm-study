aab868f9fb7cd0bb21b34fc116129544fb789fa1
Woongcheol Shin <1994_100@naver.com>
2022-02-20 23:12:56 +0900

    add problem

--- Algorithm/Binary Search/problem/2110_다시풀기.py
+++ Algorithm/Binary Search/problem/2110_다시풀기.py
@@ -1,74 +1,37 @@
-def wifi(house, spot):
-    # 변수 선언 및 할당
-    hlist = list()
-    cnt = 0
-    min = 0
-
-    # 변수 데이터 입력
-    for _ in range(house):
-        hlist.append(int(input()))
-
-    # 정렬
-    hlist = sorted(hlist)
-
-    if len(hlist) == 2:
-        print(hlist[-1] - hlist[0])
-    else:
-        # 변수 선언
-        start = 0
-        end = len(hlist)-1
-
-        # 이진 탐색 실행
-        while cnt < spot - 2:
-            # 최솟값 초기화
-            min = 0
-
-            # 이진 탐색 실행
-            if cnt == 0:
-                min, point, mid = binary_search(hlist, min, start, end)
-            else:
-                if point == start:
-                    min, point, mid = binary_search(hlist, min, start, mid)
-                else:
-                    min, point, mid = binary_search(hlist, min, mid, end)
-            
-
-            # 반복 입력
-            cnt += 1
-        return print(min)
-
-
-def binary_search(data, min, start, end):
-    # 최소값일 경우 그대로 출력
-    if min == 1:
-        return 1
+def wifi_max(N, C):
+    # 좌표 리스트 생성
+    house = list()
+    for _ in range(N):
+        house.append(int(input()))
+    house = sorted(house)
     
-    # 조건 및 중앙값 선언
-    mid = (end - start) // 2
-    cond_1 = data[end] - data[mid]
-    cond_2 = data[mid] - data[start]
+    # Gap 갱신 - 변수 선언
+    start = house[1] - house[0] # Gap 최솟값
+    end = house[-1] - house[0] # Gap 최댓값
+    result = 0
 
-    # 조건식 비교 - cond_2가 가장 가까운 경우
-    if cond_1 > cond_2:
-        if min < cond_2:
-            return cond_2, start, mid
-        else:
-            min = cond_2
-            mid = (mid + start)//2
-            binary_search[data, min, start, mid]
-    
-    # 조건식 비교 - cond_1가 가장 가까운 경우
-    else:
-        if min < cond_1:
-            return cond_1, end, mid
-        else:
-            min = cond_1
-            mid = (mid + start)//2
-            binary_search[data, min, mid+1, end]
+    # Gap 갱신 - 반복문
+    while start <= end:
+        mid = (start + end) // 2 # Gap 갱신
+        value = house[0]
+        count = 1
+
+        # 첫 번째 인덱스에서 Gap을 적용한 인덱스 찾기
+        for i in range(1, len(house)):
+            if house[i] >= value + mid:
+                value = house[i]
+                count += 1
         
+        # C개 이상 공유기를 설치할 수 있을 경우
+        if count >= C:
+            start = mid + 1
+            result = mid
+        
+        # C개 이상 공유기를 설치할 수 없을 경우
+        else:
+            end = mid - 1
+    
+    print(result)
 
 N, C = map(int, input().split())
-wifi(N, C)
-
-
-
+wifi_max(N, C)

2ccec2daf8b556601bd4c45c718930eb6e939c75
Woongcheol Shin <1994_100@naver.com>
2022-02-23 16:34:04 +0900

    add problem

---
+++ Algorithm/Dynamic Programming/problem/12865_다시 풀기.py
@@ -0,0 +1,14 @@
+def total_value(N, K):
+    dp = [[0] * (K + 1) for _ in range(N+1)]
+
+    for i in range(1, N + 1):
+        weight, value = map(int, input().split())
+        for j in range(1, K + 1):
+            if j < weight:
+                dp[i][j] = dp[i - 1][j]
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weight] + value)
+    print(dp[N][K])
+
+N, K = map(int, input().split())
+total_value(N, K)

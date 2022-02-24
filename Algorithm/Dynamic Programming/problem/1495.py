b7e5981142ac614f00cfe2ce5db787a176ef626d
Woongcheol Shin <1994_100@naver.com>
2022-02-24 13:42:18 +0900

    add problem

---
+++ Algorithm/Dynamic Programming/problem/1495.py
@@ -0,0 +1,25 @@
+def play_vol(N, S, M):
+    V = list(map(int, input().split()))
+    dp = [[False] * (M + 1) for _ in range(N+1)]
+    dp[0][S] = True
+    
+
+    for i in range(N):
+        for j in range(M+1):
+            if not dp[i][j]:
+                continue
+            if j >= V[i]:
+                dp[i+1][j - V[i]] = True
+            if M >= j + V[i]:
+                dp[i+1][j + V[i]] = True
+
+    result = -1       
+    for i in range(M, -1, -1):
+        if dp[N][i]:
+            result = i
+            break
+    
+    print(result)
+
+N, S, M = map(int, input().split())
+play_vol(N, S, M)

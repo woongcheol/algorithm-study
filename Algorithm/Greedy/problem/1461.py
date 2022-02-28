7402068650c2c881d2b1df9e3ab5eb5cadf3ab1b
Woongcheol Shin <1994_100@naver.com>
2022-02-28 15:28:53 +0900

    add problem

---
+++ Algorithm/Greedy/problem/1461.py
@@ -0,0 +1,42 @@
+import heapq
+def organize_book(location, M):
+    
+    # 책 정렬
+    left = list()
+    right = list()
+    for num in location:
+        if num > 0:
+            heapq.heappush(right, -num)
+        else:
+            heapq.heappush(left, num)
+    
+    # 가장 멀리있는 위치
+    largest = max(-left[0], -right[0])
+
+    # 1회 최대 운반량 계산
+    result = 0
+    while left:
+        result += heapq.heappop(left)*2
+        for _ in range(M-1):
+            if left:
+                heapq.heappop(left)
+            else:
+                break
+
+    while right:
+        result += heapq.heappop(right)*2
+        for _ in range(M-1):
+            if right:
+                heapq.heappop(right)
+            else:
+                break
+
+
+
+    # 합계
+    print(-result - largest)
+
+N, M = map(int, input().split())
+location = list(map(int, input().split()))
+
+organize_book(location, M)

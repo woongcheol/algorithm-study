898d14e844c009240b6e0d5c21b1537df522831a
Woongcheol Shin <1994_100@naver.com>
2022-04-05 21:14:28 +0900

    add problem

---
+++ kotlin/src/main/kotlin/1546.kt
@@ -0,0 +1,27 @@
+import java.io.*
+import java.util.StringTokenizer
+
+fun main() {
+
+    val br = BufferedReader(InputStreamReader(System.`in`))
+
+    val n = br.readLine().toInt()
+    val scoreAll = StringTokenizer(br.readLine())
+    var arr = IntArray(n)
+    var m = 0
+    var sum = 0.00
+
+    for (i in 1..n) {
+        var score = scoreAll.nextToken().toInt()
+        m = max(m, score)
+        arr[i - 1] = score
+    }
+
+    for (i in arr) {
+        sum += 100.00 * i / m
+    }
+
+    println(sum / n)
+    br.close()
+
+}

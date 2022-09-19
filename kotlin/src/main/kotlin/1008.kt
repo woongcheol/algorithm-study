dfab5f9e85cc6d0c17bc73f89112a28f15ad542b
Woongcheol Shin <1994_100@naver.com>
2022-09-19 23:29:06 +0900

    feat: solve baekjoon class 1008

--- kotlin/src/main/kotlin/1008.kt
+++ kotlin/src/main/kotlin/1008.kt
@@ -1,6 +1,7 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
+/* 1차 도전
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val (a, b) = br.readLine()!!.split(' ').map {
@@ -9,4 +10,13 @@ fun main() {
 
     val res = a / b
     println(res)
+}*/
+
+// 2차 도전
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val (a, b) = br.readLine().split(' ').map{
+        it.toDouble()
+    }
+    print(a/b)
 }

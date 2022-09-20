c0db6422892d0f4c0e300d54c0c6b5a125d46725
Woongcheol Shin <1994_100@naver.com>
2022-09-20 21:33:51 +0900

    add: backjoon class 1 1152 solved

--- kotlin/src/main/kotlin/1152.kt
+++ kotlin/src/main/kotlin/1152.kt
@@ -1,10 +1,34 @@
 import java.io.*
 import java.util.*
 
+/*
+1차 시도
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val rs = StringTokenizer(br.readLine())
 
     println(rs.countTokens())
+    br.close()
+}*/
+
+/*
+2차 시도
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    var cnt = 0
+    val word = br.readLine().split(" ").map {
+        if (it.length == 0) {
+        } else {
+            cnt += 1
+        }
+    }
+    print(cnt)
+}*/
+
+// 3차 시도
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val tokenizer = StringTokenizer(br.readLine())
+    print(tokenizer.countTokens())
     br.close()
 }

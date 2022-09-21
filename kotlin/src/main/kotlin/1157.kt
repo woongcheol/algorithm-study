1050fa4a91a866233fbfc506f3d3a5b1c1f40f79
Woongcheol Shin <1994_100@naver.com>
2022-09-21 10:16:59 +0900

    add: backjoon class 1 1157 solved

--- kotlin/src/main/kotlin/1157.kt
+++ kotlin/src/main/kotlin/1157.kt
@@ -1,6 +1,10 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.util.*
+import kotlin.collections.ArrayList
 
+/*
+1차 시도
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val str = readLine()!!
     val map = mutableMapOf<Char, Int>()
@@ -24,5 +28,31 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     } else {
         val word = map.filterValues { it == max }.keys.toString()
         println(word[1].toUpperCase())
+    }
+}*/
+
+//2차 시도
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val result = ArrayList<Int>()
+    val alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    val word = br.readLine().uppercase()
+
+    for (char in alpha) {
+        val upperCnt = word.count {
+            it == char
+        }
+        result.add(upperCnt)
+    }
+
+    val charMax = Collections.max(result)
+    val uniqueYn = result.count {
+        it == charMax
+    }
+
+    if (uniqueYn > 1) {
+        print("?")
+    } else {
+        print(alpha[result.indexOf(charMax)])
     }
 }

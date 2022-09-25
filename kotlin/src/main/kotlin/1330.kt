f9091c28fa510bb2f6552916fac776093490718d
Woongcheol Shin <1994_100@naver.com>
2022-09-25 22:34:55 +0900

    add: backjoon class 1 1330 solved

--- kotlin/src/main/kotlin/1330.kt
+++ kotlin/src/main/kotlin/1330.kt
@@ -1,6 +1,7 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
+/*1차 시도
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val (a, b) = br.readLine()!!.split(' ').map {
@@ -14,5 +15,19 @@ fun main() {
     } else {
         println("==")
     }
+}
+*/
 
+// 2차 시도
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val (a, b) = br.readLine().split(' ').map {
+        it.toInt()
     }
+
+    when {
+        (a < b) -> print("<")
+        (a > b) -> print(">")
+        (a == b) -> print("==")
+    }
+}
